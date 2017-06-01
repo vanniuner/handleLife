bfb207bd1f08596f99b4a539d42caeb52e430032
liuzijie <fred.liu@ximalaya.com>
2017-06-01 18:19:12 +0800

    测试登录

--- src/main/java/com/gpagers/cn/handle/controller/LoginController.java
+++ src/main/java/com/gpagers/cn/handle/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/wx")
+@Component
 public class LoginController {
 
     static Logger logger = LoggerFactory.getLogger(LoginController.class);
